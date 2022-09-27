10980ab189912d1e4793b2daed95ad600c45f07b
Jacob Downarowicz <jakubowarski@gmail.com>
2022-09-27 02:13:52 -0400

    First Commit

---
+++ BlackJack.js
@@ -0,0 +1,294 @@
+// KroCharon's BlackJack.js
+
+const prompt = require("prompt-sync")();
+
+function getRandomInteger(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function getRoyal() {
+  let preSelect = getRandomInteger(3);
+
+  if (preSelect == 0) {
+    royal = "Jack";
+  }
+  if (preSelect == 1) {
+    royal = "Queen";
+  }
+  if (preSelect == 2) {
+    royal = "King";
+  }
+  return royal;
+}
+
+function addToHand(arr) {
+  let temp = drawACard();
+  arr.push(temp);
+}
+
+function aceCheck(arr) {
+  let aceFound = false;
+  arr.forEach((element) => {
+    if (element == "Ace") {
+      aceFound = true;
+    }
+  });
+  return aceFound;
+}
+
+function lineBreak() {
+  console.log("- - - - - - - - - - - - -");
+}
+
+function fancyLineBreak() {
+  console.log(`~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~`);
+}
+
+function playerTurnMsg() {
+  fancyLineBreak();
+  console.log("Player turn: would you like to hit [1] <or> stay [2]");
+}
+
+function dealerTurnMsg() {
+  console.log("Dealer turn: would you like to hit [1] <or> stay [2]");
+}
+
+function bust() {
+  console.log("You went over! BUST!");
+}
+
+function playerInfo(arr) {
+  let pI = addTotal(arr);
+  fancyLineBreak();
+  console.log("Player drew: [ " + arr + " ]");
+  fancyLineBreak();
+  console.log("Player's hand totals: " + "< " + pI + " >");
+  return pI;
+}
+
+function dealerInfo(arr) {
+  let dI = addTotal(arr);
+  fancyLineBreak();
+  console.log("Dealer drew: [ " + arr[0] + " [?]" + " ]");
+  fancyLineBreak();
+  //console.log("Dealer's hand totals: " + "< " + dI + " >");
+  return dI;
+}
+
+function notCompleteConclusion(val1, val2) {
+  let v1 = addTotal(val1);
+  let v2 = addTotal(val2);
+
+  if (v1 > v2) {
+    fancyLineBreak();
+    console.log("Player wins!");
+    fancyLineBreak();
+  } else if (v2 > v1) {
+    fancyLineBreak();
+    console.log("Dealer wins!");
+    fancyLineBreak();
+  } else if ((v1 = v2)) {
+    fancyLineBreak();
+    console.log("    DRAW!");
+    fancyLineBreak();
+  }
+  //notCompleteConclusion(userCards, dealerCards);
+}
+
+function drawACard() {
+  let preCard = getRandomInteger(14); // 14, 1 for Ace testing
+  let finalCard;
+  if (preCard == 0) {
+    finalCard = "Ace";
+  } else if (preCard == 11 || preCard == 12 || preCard == 13) {
+    finalCard = getRoyal();
+  } else {
+    finalCard = preCard;
+  }
+  return finalCard;
+}
+
+function addTotal(arr) {
+  let total = 0;
+  let ac = 0;
+
+  arr.forEach((element) => {
+    let count = 0;
+    if (element == "Jack" || element == "Queen" || element == "King") {
+      count = 10;
+    } else if (element == "Ace") {
+      count = 11;
+      ac++;
+    } else {
+      count = element;
+    }
+    total += count;
+  });
+
+  // If the total exceeds 21, and you possess and ace, count Ace as a one.
+  // Repeat for each Ace detected.
+  // For each Ace that you have available,
+  // provided you are over 21, you have the ability to
+  // instead count it as a 1 (subtract 10)
+
+  if (total > 21 && aceCheck(arr) == true) {
+    for (i = 0; i < ac; i++) {
+      if (total > 21) {
+        total -= 10;
+      }
+    }
+    // If you somehow manage to go over even with the Ace deduction,
+    // eg. Jack + Jack + 4 Aces (44 - 40) = 22,
+    // display an error and call Bust();
+
+    if (total > 21) {
+      bust();
+    } else {
+      console.log("You would have gone over, but an Ace was counted as one.");
+      console.log("You had: " + ac + " total Ace(s)");
+    }
+  }
+  return total;
+}
+
+function dealTable(array1, array2) {
+  addToHand(array1); // One to Player,
+  addToHand(array2); // One to Dealer,
+  addToHand(array1); // One to Player,
+  addToHand(array2); // One to Dealer.
+  return array1, array2;
+}
+
+function compareHand(arr1, arr2) {
+  if (arr1 > arr2) {
+    console.log("Player wins!");
+  } else if (arr2 > arr1) {
+    console.log("Dealer wins!");
+  } else if (arr1 == arr2) {
+    console.log("PUSH");
+  }
+}
+
+function earlyCheck(total) {
+  let cont = true;
+  if (total == 21) {
+    cont = false;
+  }
+  return cont;
+}
+
+function tableSet(arr1, arr2, t1, t2) {
+  let totArr = [];
+  t1 = playerInfo(arr1);
+  t2 = dealerInfo(arr2);
+  totArr.push(t1, t2);
+  return totArr;
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+function blackJack() {
+  let gameOngoing = true;
+  let keepPlay = true;
+  let userCards = [];
+  let userTotal;
+  let dealerCards = [];
+  let dealerTotal;
+
+  let Info;
+  let temp = 0;
+  let fdi;
+
+  dealTable(userCards, dealerCards);
+  Info = tableSet(userCards, dealerCards, userTotal, dealerTotal);
+
+  userTotal = Info[0];
+  dealerTotal = Info[1];
+
+  if (earlyCheck(userTotal) == false) {
+    // Assuming we instantly compare hands.
+    console.log("Player has BLACKJACK");
+    compareHand(userTotal, dealerTotal);
+    gameOngoing = false;
+    keepPlay = false;
+  } else if (earlyCheck(dealerTotal) == false) {
+    console.log("DEALER has BLACKJACK");
+    compareHand(userTotal, dealerTotal);
+    gameOngoing = false;
+    keepPlay = false;
+  } else if (
+    earlyCheck(userTotal) == false &&
+    earlyCheck(dealerTotal) == false
+  ) {
+    console.log("PUSH - BOTH PLAYERS HAVE BLACKJACK");
+    gameOngoing = false;
+    keepPlay = false;
+  }
+
+  fancyLineBreak();
+
+  while (keepPlay) {
+    playerTurnMsg();
+    stat = prompt("HIT [1] | STAY [2] : ");
+    if (stat == 1) {
+      fancyLineBreak();
+      console.log("Player has chosen to [HIT]");
+      addToHand(userCards);
+      temp = playerInfo(userCards);
+      userTotal = temp;
+      if (temp == 21) {
+        keepPlay = false;
+        gameOngoing = false;
+        console.log("BLACKJACK! Player wins!");
+      }
+      if (temp > 21) {
+        keepPlay = false;
+        gameOngoing = false;
+        bust();
+        console.log("Dealer wins!");
+      }
+    } else if (stat == 2) {
+      fancyLineBreak();
+      keepPlay = false;
+      console.log("Player has chosen to [STAY]");
+      console.log("Player holds: [ " + userCards + " ]"); // Can be put into a function for both Player and Dealer.
+      console.log("Value totals: " + "< " + userTotal + " >");
+    }
+  } // End of player turn.
+
+  fdi = addTotal(dealerCards);
+  fancyLineBreak();
+  console.log("Dealer drew: [ " + dealerCards + " ]");
+  fancyLineBreak();
+  console.log("Dealer's hand totals: " + "< " + fdi + " >");
+
+  while (gameOngoing) {
+    if (dealerTotal >= 17) {
+      // Condition does not need to account for over 21 since first hand held at time of executing.
+      console.log("The Dealer chooses to [STAND]");
+      compareHand(userTotal, dealerTotal);
+      gameOngoing = false;
+    } else if (dealerTotal < 17) {
+      console.log("The Dealer chooses to [HIT]");
+      addToHand(dealerCards);
+      console.log("Dealer now holds: " + dealerCards);
+
+      console.log(
+        "Dealer's hand totals: " + "< " + addTotal(dealerCards) + " >"
+      );
+
+      dealerTotal = addTotal(dealerCards);
+
+      if (addTotal(dealerCards) > 21) {
+        bust();
+        gameOngoing = false;
+      }
+    }
+  }
+}
+
+blackJack();
---
+++ Craps.js
@@ -0,0 +1,68 @@
+//KroCharon's Craps.js
+
+// Math.floor rounds down no matter what, so 3.6 => 3
+// If we're going for a dice for example, pass 7 instead of 6.
+
+function getRandomInteger(max) {
+  return Math.floor(Math.random() * max);
+}
+function crapsSimulation() {
+  rollout = 7; // 6 possible outcomes (for two dice), when floored
+  let continuePlay = true;
+
+  let point = 0;
+  let once = true;
+
+  while (continuePlay) {
+    let roll1 = getRandomInteger(rollout);
+    let roll2 = getRandomInteger(rollout);
+
+    if (roll1 == 0) {
+      // Don't know how to make math.random exclude zero, so just assume 1
+      roll1 += 1;
+    }
+    if (roll2 == 0) {
+      // Don't know how to make math.random exclude zero, so just assume 1
+      roll2 += 1;
+    }
+
+    let sum = roll1 + roll2;
+
+    console.log("You rolled " + roll1 + " + " + roll2 + " = " + sum);
+    // If we rolled once already, 7 dictates a loss.
+    if (((sum == 2 || sum == 3 || sum == 12) && once) || (sum == 7 && !once)) {
+      console.log("Craps, Better Luck Next Time, You lose");
+      continuePlay = false;
+    }
+    // If we haven't rolled yet, 7 grants a win.
+    else if ((sum == 7 && once) || (sum == 11 && once) || sum == point) {
+      console.log("Congratulations, You win");
+      continuePlay = false;
+      // The first time you roll and don't complete the game,
+      // set the point, and output once (only).
+      // You have no longer rolled once.
+    } else {
+      if (point == 0) {
+        point = sum;
+        once = false;
+        console.log("Point is (established) set to " + point);
+      }
+    }
+  }
+}
+crapsSimulation();
+
+//                Task 2:
+//                This task is required to create Craps, which is a popular dice game played in casinos. You are
+//                supposed to write a program to play a variation of the game, as follows:
+//              • Roll two dice. (Each roll should produce two random numbers between 1 to 6)
+//              • Each die has six faces representing values 1, 2, …, and 6, respectively.
+//              • Check the sum of the two dice. If the sum is 2, 3, or 12 (your program should display
+//                craps), you lose the game.
+//              • If the sum of the two dice is 7 or 11 (your program should display naturals), you win the
+//                game.
+//              • If the sum of two dice is any value (i.e., 4, 5, 6, 8, 9, or 10), your program should establish
+//                a point in the game (meaning store that sum). Continue to roll the dice until the sum is
+//                either a 7 or the same point value which was established. If rolled sum is 7, you lose the
+//                game. Otherwise, if the rolled sum is equal to established point you win.
+//                Your program acts as a single player.
